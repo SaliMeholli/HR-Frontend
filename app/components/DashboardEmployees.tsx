8c5fa6ae2467cf53f431ff93c821b7af064e1306
SaliMeholli <salidev.ks@gmail.com>
2024-08-16 01:02:43 +0200

    feat: add admin dashboard

---
+++ app/components/DashboardEmployees.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useState } from "react";
+
+type Employee = {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    joinedAt: string;
+    role: string;
+  };
+  
+  type DashboardEmployeesProps = {
+    employees: Employee[];
+  };
+  
+  function DashboardEmployees({ employees }: DashboardEmployeesProps) {
+    console.log(employees);
+    const employeesCount = employees.length;
+
+  const handleDelete = async (id: number) => {
+    console.log(`Employee with id ${id} deleted`);
+
+    try {
+      const response = await fetch("http://localhost:4000/users", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }), // Send the id in the body
+      });
+
+      if (response.ok) {
+        console.log(`Employee with id ${id} was successfully deleted.`);
+        // Optionally, you can update the state to remove the deleted employee from the list
+        // setEmployeesData((prev) => prev.filter((employee) => employee.id !== id));
+      } else {
+        console.error("Failed to delete employee:", await response.text());
+      }
+    } catch (error) {
+      console.error("Error occurred while deleting employee:", error);
+    }
+  };
+
+  return (
+    <div className="w-full p-10">
+      <div className="bg-white p-10 rounded-lg">
+        <div className="flex justify-between mb-5">
+          <div>
+            <h1>Last Employees</h1>
+            <p className="text-lg">
+              <span className="font-bold">
+                {employeesCount} Total Employees
+              </span>
+              , proceed to see them
+            </p>
+          </div>
+          <div>
+            <h1 className="text-2xl text-center">
+              <span className="font-bold">
+                {employeesCount} <br /> Employees
+              </span>
+            </h1>
+          </div>
+        </div>
+        <table className="min-w-full  bg-white">
+          <thead>
+            <tr>
+              <th className="py-2">ID</th>
+              <th className="py-2">Name</th>
+              <th className="py-2">Surname</th>
+              <th className="py-2">Email</th>
+              <th className="py-2">Joined At</th>
+              <th className="py-2">Role</th>
+              <th className="py-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {employees.map((employee) => (
+              <tr key={employee.id} className="border-b ">
+                <td className="py-10 text-center">{employee.id}</td>
+                <td className="py-10 text-center">{employee.name}</td>
+                <td className="py-10 text-center">{employee.surname}</td>
+                <td className="py-10 text-center">{employee.email}</td>
+                <td className="py-10 text-center">
+                  {" "}
+                  {new Date(employee.joinedAt).toLocaleDateString()}
+                </td>
+                <td className="py-10 text-center">{employee.role}</td>
+                <td className="py-10 text-center">
+                  <button
+                    className="bg-red-500 text-white px-3 py-1 rounded"
+                    onClick={() => handleDelete(employee.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardEmployees;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import DashboardNavbar from "../components/DashboardNavbar";
+import DashboardEmployees from "../components/DashboardEmployees";
+
+function Dashboard() {
+  const [employeesData, setEmployeesData] = useState([]);
+  console.log(employeesData)
+  async function getData() {
+    try {
+      const res = await fetch("http://localhost:4000/users");
+      
+      if (!res.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await res.json();
+      setEmployeesData(data);
+    } catch (error) {
+      console.error("Error fetching employees data:", error);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  return (
+    <div className="flex bg-gray-300 ">
+      <div className="">
+        <Sidebar />
+      </div>
+      <div className="w-full">
+        <DashboardNavbar />
+        <DashboardEmployees employees={employeesData}/>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
