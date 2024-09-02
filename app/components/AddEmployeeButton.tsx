0bf2e76bd0b51f80ca5c385f31dafda1e739335e
gjanuzii <januceng@gmail.com>
2024-09-03 00:27:36 +0200

    Update AddEmployeeButton.tsx

--- app/components/AddEmployeeButton.tsx
+++ app/components/AddEmployeeButton.tsx
@@ -3,3 +3,12 @@ import React from 'react';
 const AddEmployeeButton = () => {
   return (
     <a
+      href="../AddEmployee"
+      className="p-2 bg-blue-500 text-white rounded inline-block"
+    >
+      Add Employee
+    </a>
+  );
+};
+
+export default AddEmployeeButton;
