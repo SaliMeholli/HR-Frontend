fdd0055c994f35cae96ee6b1c47f773a65d4e69c
gjanuzii <januceng@gmail.com>
2024-09-03 00:27:20 +0200

    AddEmployee
    E kom shtu butonin me add employee qe tnavigon te faqja per mi jep tdhanat

---
+++ app/AddEmployee/page.tsx
@@ -0,0 +1,13 @@
+// pages/AddEmployee/page.tsx
+import React from 'react';
+
+const AddEmployeePage = () => {
+  return (
+    <div>
+      <h1>Add Employee</h1>
+      {/* Add your form or content for adding an employee here */}
+    </div>
+  );
+};
+
+export default AddEmployeePage;
