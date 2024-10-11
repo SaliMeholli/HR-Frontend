00bcba70998f75da1d95174c36aee355ea54742e
SaliMeholli <salidev.ks@gmail.com>
2024-10-11 23:31:44 +0200

    add careers page

---
+++ app/protected-route.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { User } from "./types/User";
+import { PropsWithChildren } from "react";
+import { useAuth } from "./context/AuthProvider";
+
+type ProtectedRouteTypes = PropsWithChildren<{
+  allowedRoles?: User["role"][];
+}>;
+
+function ProtectedRoute({ allowedRoles, children }: ProtectedRouteTypes) {
+  const { currentUser } = useAuth();
+
+  if (currentUser === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (
+    currentUser === null ||
+    (allowedRoles && !allowedRoles.includes(currentUser.role))
+  ) {
+    return <div>This is a protected route, only the admin can see this.</div>;
+  }
+
+  return <>{children}</>;
+}
+
+export default ProtectedRoute;
